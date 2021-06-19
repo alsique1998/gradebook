461853295cb34ddb0140e2bc13fe4c77230140b7
Abner Landim Siqueira <abner.l.siqueira@exxonmobil.com>
2021-06-19 12:08:11 -0300

    chore: first commit

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String arg = args?.Length > 0 ? args?[0] : "Abner";  
+            Console.WriteLine($"Hello, {arg}! Nice to meet you");
+        }
+    }
+}
